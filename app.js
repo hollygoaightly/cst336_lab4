697d3fa7bc2bd922395b32e69bc84865a02e9b37
EC2 Default User <ec2-user@ip-172-31-17-249.us-west-1.compute.internal>
2020-11-19 08:54:38 +0000

    finished

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.get("/venus", function(req, res){
     res.render("venus.html"); 
 });
 
+app.get("/earth", function(req, res){
+    res.render("earth.html"); 
+});
+
 // starting server
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Express server is running...");
